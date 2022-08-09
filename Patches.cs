2088ec0c0ef6a34e0adc9c9a31ea01d8847c9e31
GruffCassquatch <cassanda.agius@gmail.com>
2022-08-09 15:55:58 +1000

    Add project files.

---
+++ Patches.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+
+namespace SilentProtagonist
+{
+    internal static class Patches
+    {
+        [HarmonyPatch(typeof(PlayerVoice), "CanPlayPlayerVoiceEvents")]
+        private static class SilenceProtagonist
+        {
+            internal static bool Prefix(ref bool __result)
+            {
+                __result = false;
+                return false;
+            }
+        }
+    }
+}
